ad5f5992ed06d59610c877050d458757e12e16cc
unknown <vanisgmfs@gmail.com>
2022-05-14 09:45:12 -0300

    adicionando array

---
+++ cypress/integration/array.spec.js
@@ -0,0 +1,4 @@
+// Criar array com nome do time
+// Exportar o arquivo
+// Importar no Introdução.spec
+
--- cypress/integration/introducao.spec.js
+++ cypress/integration/introducao.spec.js
@@ -1,14 +1,21 @@
 /// <reference types="cypress" />
 
+// import  resultado from "./soma.spec"
+
+import {soma, sub, div, mult} from './soma.spec'
+
 it('functions', function() {
 
     
     const helloWorld = () => "Curso Cypress"
 
     const somaValores = () => {
-        return 1 + 2
+        const estudarOque = () => "Cypress Pessoal"
+        return estudarOque()
+        
     }
-    
+     console.log(somaValores())
+
     function windowJs() {
         console.log(this)
     }
@@ -19,9 +26,16 @@ it('functions', function() {
 
     console.log('function', windowJs())
     console.log('arrow function', windowJsArrowFunction())
+    console.log('O resultado da soma é: ' + soma)
+    console.log('O resultado da subtração é: ' + sub)
+    console.log('O resultado da divisão é: ' + Math.round(div))
+    console.log('O resultado da multiplicação é: ' + mult)
+
     //console.log(helloWorld())
     //console.log(somaValores())
+    //console.log(estudarOque())
     
+   
 
 })
 
