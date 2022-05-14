6a21fbb7a79fc91d141d34a5c7e03979a2e413c0
Vitor da Silva Vicente <vitor.vicente@solutis.net.br>
2022-05-14 11:39:58 -0300

    Aula 2 foi realizado a introducao da funcao expect do cypress

---
+++ cypress/integration/expects.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('Expects', function() {
+    it('Equality == ', function() {
+        let idadeMaiorque18anos = 18
+ 
+        expect(idadeMaiorque18anos).to.be.equals(18)
+        expect(idadeMaiorque18anos).equals(18)
+        expect(idadeMaiorque18anos).eqls(18)
+
+        expect(idadeMaiorque18anos).not.to.be.equals(17)
+        expect(idadeMaiorque18anos).not.equals(17)
+        expect(idadeMaiorque18anos).not.eqls(17)
+
+        let nomes = ['Joao','Maria','Catarina','Bruno']
+        expect(nomes[0]).equals('Joao').and.contains('Joa')
+
+        // se a maria e igual a maria
+        // se a catarina e igual a catarina
+        // se o bruno e igual o bruno
+    })
+})
